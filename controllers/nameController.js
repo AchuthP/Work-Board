835feee81223f1bd84be5fdb9de9751b818d1a5b
achuthp <achuthpalassery@gmail.com>
2019-10-04 22:34:04 +0530

    Added signup,login,teacherpost modules.Yet to add user profile.

--- controllers/nameController.js
+++ controllers/nameController.js
@@ -2,8 +2,24 @@
 nameController = {
     name: (req, res ,next) => {
         let name = req.params.name;
-        res.render('demo', { name: name, address: 'Kothanur' });
+        res.render('demo', { title: "title"});
+    },
+    // get about view
+    getAbout: (req, res, next) => {
+        console.log("function called")
+        res.render('about', { title: "title"});
+    },
+    // get tpost view
+    getTpost: (req,res,next) => {
+        res.render('tpost', {title:"title"})
+    },
+    // get login view
+    getLogin: (req,res,next) => {
+        res.render('login', {title:"title"})
+    },
+    getSignup: (req,res,next) => {
+        res.render('signup', {title:"title"})
     }
-}
+} 
 
 module.exports = nameController;
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Work Board' });
 });
 
-router.get('/:name', nameController.name)
+router.get('/achu', nameController.name)
+
+router.get('/about', nameController.getAbout)
+
+router.get('/teacherpost',nameController.getTpost)
+
+router.get('/signup', nameController.getSignup)
+
+router.get('/login', (req, res) => {
+  console.log("HITs")
+  res.render('login');
+})
+
+
+
 
 module.exports = router;
